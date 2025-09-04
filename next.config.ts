760aba469078765a7d5d2e05501ed81373da3437
mousumiahmed <mousumiara.ahmed@gmail.com>
2025-09-03 18:58:20 -1200

    Save local changes before merge

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // enables static HTML export
+  // optional: set a base path if deploying to GitHub Pages
+  basePath: '/mousumi_official_business_app', 
 };
 
-export default nextConfig;
+module.exports = nextConfig;
